c47d565bf2fc9c2dc052e956831432cb7a56aaa1
Kostya Linou <klinou@confluent.io>
2023-03-28 14:39:50 -0700

    Follow up to #220 (#221)

--- cmk/v2/model_cmk_v2_cluster_spec_config_one_of.go
+++ cmk/v2/model_cmk_v2_cluster_spec_config_one_of.go
@@ -32,10 +32,9 @@ import (
 
 // CmkV2ClusterSpecConfigOneOf - struct for CmkV2ClusterSpecConfigOneOf
 type CmkV2ClusterSpecConfigOneOf struct {
-	CmkV2Basic      *CmkV2Basic
-	CmkV2Dedicated  *CmkV2Dedicated
-	CmkV2Enterprise *CmkV2Enterprise
-	CmkV2Standard   *CmkV2Standard
+	CmkV2Basic     *CmkV2Basic
+	CmkV2Dedicated *CmkV2Dedicated
+	CmkV2Standard  *CmkV2Standard
 }
 
 // CmkV2BasicAsCmkV2ClusterSpecConfigOneOf is a convenience function that returns CmkV2Basic wrapped in CmkV2ClusterSpecConfigOneOf
@@ -46,11 +45,6 @@ func CmkV2BasicAsCmkV2ClusterSpecConfigOneOf(v *CmkV2Basic) CmkV2ClusterSpecConf
 // CmkV2DedicatedAsCmkV2ClusterSpecConfigOneOf is a convenience function that returns CmkV2Dedicated wrapped in CmkV2ClusterSpecConfigOneOf
 func CmkV2DedicatedAsCmkV2ClusterSpecConfigOneOf(v *CmkV2Dedicated) CmkV2ClusterSpecConfigOneOf {
 	return CmkV2ClusterSpecConfigOneOf{CmkV2Dedicated: v}
-}
-
-// CmkV2EnterpriseAsCmkV2ClusterSpecConfigOneOf is a convenience function that returns CmkV2Enterprise wrapped in CmkV2ClusterSpecConfigOneOf
-func CmkV2EnterpriseAsCmkV2ClusterSpecConfigOneOf(v *CmkV2Enterprise) CmkV2ClusterSpecConfigOneOf {
-	return CmkV2ClusterSpecConfigOneOf{CmkV2Enterprise: v}
 }
 
 // CmkV2StandardAsCmkV2ClusterSpecConfigOneOf is a convenience function that returns CmkV2Standard wrapped in CmkV2ClusterSpecConfigOneOf
@@ -92,18 +86,6 @@ func (dst *CmkV2ClusterSpecConfigOneOf) UnmarshalJSON(data []byte) error {
 		}
 	}
 
-	// check if the discriminator value is 'Enterprise'
-	if jsonDict["kind"] == "Enterprise" {
-		// try to unmarshal JSON data into CmkV2Enterprise
-		err = json.Unmarshal(data, &dst.CmkV2Enterprise)
-		if err == nil {
-			return nil // data stored in dst.CmkV2Enterprise, return on the first match
-		} else {
-			dst.CmkV2Enterprise = nil
-			return fmt.Errorf("Failed to unmarshal CmkV2ClusterSpecConfigOneOf as CmkV2Enterprise: %s", err.Error())
-		}
-	}
-
 	// check if the discriminator value is 'Standard'
 	if jsonDict["kind"] == "Standard" {
 		// try to unmarshal JSON data into CmkV2Standard
@@ -140,18 +122,6 @@ func (dst *CmkV2ClusterSpecConfigOneOf) UnmarshalJSON(data []byte) error {
 		}
 	}
 
-	// check if the discriminator value is 'cmk.v2.Enterprise'
-	if jsonDict["kind"] == "cmk.v2.Enterprise" {
-		// try to unmarshal JSON data into CmkV2Enterprise
-		err = json.Unmarshal(data, &dst.CmkV2Enterprise)
-		if err == nil {
-			return nil // data stored in dst.CmkV2Enterprise, return on the first match
-		} else {
-			dst.CmkV2Enterprise = nil
-			return fmt.Errorf("Failed to unmarshal CmkV2ClusterSpecConfigOneOf as CmkV2Enterprise: %s", err.Error())
-		}
-	}
-
 	// check if the discriminator value is 'cmk.v2.Standard'
 	if jsonDict["kind"] == "cmk.v2.Standard" {
 		// try to unmarshal JSON data into CmkV2Standard
@@ -177,10 +147,6 @@ func (src CmkV2ClusterSpecConfigOneOf) MarshalJSON() ([]byte, error) {
 		return json.Marshal(&src.CmkV2Dedicated)
 	}
 
-	if src.CmkV2Enterprise != nil {
-		return json.Marshal(&src.CmkV2Enterprise)
-	}
-
 	if src.CmkV2Standard != nil {
 		return json.Marshal(&src.CmkV2Standard)
 	}
@@ -196,10 +162,6 @@ func (obj *CmkV2ClusterSpecConfigOneOf) GetActualInstance() interface{} {
 
 	if obj.CmkV2Dedicated != nil {
 		return obj.CmkV2Dedicated
-	}
-
-	if obj.CmkV2Enterprise != nil {
-		return obj.CmkV2Enterprise
 	}
 
 	if obj.CmkV2Standard != nil {
